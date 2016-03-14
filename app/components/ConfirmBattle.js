8f23c70033c70a8474a7a32da6796c80e2d37820
Andy Bolos <andybolos@gmail.com>
2016-03-14 16:41:42 -0600

    adds PropTypes to ConfirmBattle

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,4 +1,6 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+
 
 function puke (obj) {
     return <pre>{JSON.stringify(obj, null, ' ')}</pre>
@@ -8,6 +10,11 @@ function ConfirmBattle(props) {
     return props.isLoading === true
         ? <p> Patience... It is LOADING! </p>
         : <div> Confirm Battle! {puke(props)}</div>
+}
+
+ConfirmBattle.PropTypes = {
+    isLoading: PropTypes.bool.isRequired,
+    playersInfo: PropTypes.array.isRequired
 }
 
 module.exports = ConfirmBattle;
