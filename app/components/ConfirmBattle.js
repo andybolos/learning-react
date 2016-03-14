489f43b9e1427dfea82a439314e0f2dc783c7be5
Andy Bolos <andybolos@gmail.com>
2016-03-14 16:38:14 -0600

    display the object to the screen in JSON form

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,9 +1,13 @@
 var React = require('react');
 
+function puke (obj) {
+    return <pre>{JSON.stringify(obj, null, ' ')}</pre>
+}
+
 function ConfirmBattle(props) {
     return props.isLoading === true
         ? <p> Patience... It is LOADING! </p>
-        : <p> Confirm Battle! </p>
+        : <div> Confirm Battle! {puke(props)}</div>
 }
 
 module.exports = ConfirmBattle;
